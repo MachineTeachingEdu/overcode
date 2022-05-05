b72823d493f64de5c342d27eb4d21ae52dce72e6
artsasse <artsasse@gmail.com>
2022-05-05 17:18:02 -0300

    data for tests

---
+++ ui/data/tidyData/aluno1.py
@@ -0,0 +1,11 @@
+def breakeven():
+    anos=0
+    pop_a=80000
+    pop_b=200000
+    taxa_a=0.03
+    taxa_b=0.015
+    while pop_a<pop_b:
+        pop_a=pop_a+pop_a*taxa_a
+        pop_b=pop_b+pop_b*taxa_b
+        anos+=1
+    return anos
