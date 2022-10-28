912580bbc7d194b679f8bcd0edb92366bf6dffe0
artsasse <artsasse@gmail.com>
2022-10-28 10:29:25 -0300

    add flag to skip preprocessing

--- run.sh
+++ run.sh
@@ -5,11 +5,24 @@ set -e  # Interrompe o script se houver erro
 TARGET_PATH="${PWD}/ui"
 FUNCTION_NAME="posLetra"
 
-# Executa as soluções dos alunos
-python src/run_pipeline.py $TARGET_PATH --run-pre -n $FUNCTION_NAME
+# Verifica se foi passada a flag '-a', indicando que apenas a análise deve ser feita
+while getopts 'a' OPTION; do
+    case "$OPTION" in
+        a) 
+            # Executa a análise das soluções
+            python src/run_pipeline.py $TARGET_PATH --run-pipeline -d
+            ;;
+    esac
+done
 
-# Executa a análise das soluções
-python src/run_pipeline.py $TARGET_PATH --run-pipeline -d
+# Se nao foram passadas flags, executa o pipeline completo
+if (( $OPTIND == 1 ));
+then
+    # Executa as soluções dos alunos
+    python src/run_pipeline.py $TARGET_PATH --run-pre -n $FUNCTION_NAME
+    # Executa a análise das soluções
+    python src/run_pipeline.py $TARGET_PATH --run-pipeline -d
+fi
 
 # Roda o servidor da interface
 cd ./ui
