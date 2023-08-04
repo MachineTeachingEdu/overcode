6d107800ce9519b235bbada3c6c57fd208cc9880
artsasse <artsasse@gmail.com>
2023-08-04 17:34:19 -0300

    adapt future prompt to the newly created python scripts

--- scripts/analyze_problem.py
+++ scripts/analyze_problem.py
@@ -7,21 +7,21 @@ The user should input only the problem id (lets call <id>).
 
 Then the script checks the '../problems_data/problem_<id>' directory (note that it doesnt always exist, neither the problems_data directory)
 
-if the problem_<id> dir has a subdir called 'output', then the script should run replace_output_data.py with the <id> as argument.
-Then it should run the runServer.sh script(or run_interface.py).
+if the problem_<id> dir has a subdir called 'output', then the script should run replace_output_data function (from replace_output_data.py) with the <id> and '../ui' as argument.
+Then it should run the run_interface function from run_interface.py.
 
 if the problem_<id> dir doesnt have a subdir called 'output', but has a subdir called 'data', then the script should:
     - get the function name and save it in a variable (lets call it funcname)
     - call run_pipeline function (from run_pipeline.py) with base_dir=../problems_data/problem_<id> and funcname=funcname.
     - then it should call replace_output_data function (from replace_output_data.py) with the <id> as argument.
-    - Then it should run the runServer.sh script(or run_interface.py).
+    - Then it should run the run_interface function from run_interface.py.
 
 if the problem_<id> dir doesnt have a subdir called 'output' nor 'data', then the script should:
     - call the get_problem_data function (from get_problem_data.py) with the <id> as argument.
     - get the function name and save it in a variable (lets call it funcname)
     - call run_pipeline function (from run_pipeline.py) with base_dir=../problems_data/problem_<id> and funcname=funcname.
     - then it should call replace_output_data function (from replace_output_data.py) with the <id> as argument.
-    - Then it should run the runServer.sh script (or run_interface.py).
+    - Then it should run the run_interface function from run_interface.py.
 '''
 
 
