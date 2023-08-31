5470938e7121fdb01b3b42110b2bd935746469e9
artsasse <artsasse@gmail.com>
2023-08-31 17:27:33 -0300

    minor fixes

--- src/external/var_renamer.py
+++ src/external/var_renamer.py
@@ -1,5 +1,5 @@
-from redbaron import RedBaron
 import ast
+
 
 class IdentifierRenamer(ast.NodeTransformer):
     def visit_Name(self, node):
@@ -39,12 +39,12 @@ class IdentifierRenamer(ast.NodeTransformer):
             node.name = self.mapping[node.name]
         self.generic_visit(node)
         return node
-    
+
     def visit_Global(self, node):
         # Check if any of the global names are in the mapping
         node.names = [self.mapping.get(name, name) for name in node.names]
         return node
-    
+
     def visit_Nonlocal(self, node):
         # Check if any of the nonlocal names are in the mapping
         node.names = [self.mapping.get(name, name) for name in node.names]
@@ -67,63 +67,28 @@ class VariableNameClashException(Exception):
         super().__init__(self.message)
 
 
+# Making the rename method flexible
 def rename_var(src_code: str, var_name: str, new_var_name: str) -> str:
     return ast_rename_var(src_code, var_name, new_var_name)
-
-
-def redbaron_rename_var(src_code: str, var_name: str, new_var_name: str) -> str:
-    """
-    Rename variables in a provided source code.
-
-    Args:
-        src_code     -- string representing source code of a syntactically-correct
-                        Python program
-        var_name     -- variable to rename
-        new_var_name -- the string to rename it to. It's the caller's
-                        responsibility to confirm that this is a legal
-                        Python identifier
-    Returns:
-        A new string similar to src_code except with ALL OCCURRENCES
-        of var_name renamed to new_var_name
-
-        Raises a VariableNameClashException if it detects a clash with the new variable name
-        provided.
-
-    PS:
-        Docs for RedBaron can be found on: https://redbaron.readthedocs.io/en/latest/tuto.html
-    """
-
-
-    red = RedBaron(src_code)
-
-    # The "name" arg makes it search only for NameNodes in the Syntax Tree
-    if red.find("name", value=new_var_name):
-        raise VariableNameClashException(new_var_name)
-    else:
-        vars = red.find_all("name", value=var_name)
-        for var in vars:
-            var.value = new_var_name
-        return red.dumps()
 
 
 def ast_rename_var(src_code: str, var_name: str, new_var_name: str) -> str:
     # Parse the code into an AST
     tree = ast.parse(src_code)
-    
+
     # Create a VariableRenamer object
     renamer = IdentifierRenamer()
 
     # Create a mapping of old variable names to new variable names
     mapping = {var_name: new_var_name}
-    
+
     # Set the mapping on the renamer
     renamer.mapping = mapping
-    
+
     # Use the renamer to transform the AST
     renamer.visit(tree)
-    
+
     # Generate the new code from the transformed AST
     new_code = ast.unparse(tree)
-    
-    return new_code
 
+    return new_code
